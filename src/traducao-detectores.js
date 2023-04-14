7c8315812c4d3746d56b22da078c8a67fd341637
alexbjr369 <alexbjr369@gmail.com>
2023-04-14 08:54:39 -0300

    commit

--- src/traducao-detectores.js
+++ src/traducao-detectores.js
@@ -1,17 +1,3 @@
-/* const isElementLoaded = async (selector) => {
-  while (document.querySelector(selector) === null) {
-    await new Promise((resolve) => requestAnimationFrame(resolve));
-  }
-  return document.querySelector(selector);
-};
-
-isElementLoaded('.step-2').then((selector) => {
-  const p = selector.querySelector('p');
-  if (p) {
-    p.innerHTML = 'Marque <strong>sólo una</strong> de las opciones siguientes:';
-  }
-});
- */
 setInterval(() => {
   const change = document.querySelector('.step-2 .init-quiz p');
   if (change) change.innerHTML = 'Marque <strong>sólo una</strong> de las opciones siguientes:';
